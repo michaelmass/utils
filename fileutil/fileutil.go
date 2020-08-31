7df7e1a183b75782ca9f2cce82a92a8238cb55f0
unknown <mmasson@stingray.com>
2020-08-31 19:26:04 -0400

    update file util

---
+++ fileutil/fileutil.go
@@ -0,0 +1,7 @@
+package fileutil
+
+// Utility constants for managing files
+const (
+	DefaultFilePerm = 0644
+	DefaultDirPerm  = 0755
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/michaelmass/utils
+
+go 1.15
