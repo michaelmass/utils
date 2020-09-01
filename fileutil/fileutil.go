b2854532e835d583fba746c2bb7586845265e8f7
unknown <mmasson@stingray.com>
2020-08-31 20:35:27 -0400

    added fileutil copy function

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -1,7 +1,30 @@
 package fileutil
 
+import (
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+)
+
 // Utility constants for managing files
 const (
 	DefaultFilePerm = 0644
 	DefaultDirPerm  = 0755
 )
+
+// Copy copies a file from a source to a destination
+func Copy(source string, destination string) error {
+	content, err := ioutil.ReadFile(source)
+
+	if err != nil {
+		return errors.Wrapf(err, "Error reading file %s", source)
+	}
+
+	err = ioutil.WriteFile(destination, content, DefaultFilePerm)
+
+	if err != nil {
+		return errors.Wrapf(err, "Error writing file %s", destination)
+	}
+
+	return nil
+}
