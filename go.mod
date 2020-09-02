b86b5c4826fd7d74e1e14a623ab69769e59c6e99
unknown <mmasson@stingray.com>
2020-09-02 00:33:18 -0400

    added templates util

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/michaelmass/utils
 go 1.15
 
 require (
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
---
+++ tplutil/tplutil.go
@@ -0,0 +1,55 @@
+package tplutil
+
+import (
+	"io"
+	"io/ioutil"
+	templ "text/template"
+
+	"github.com/Masterminds/sprig"
+)
+
+var funcMap = sprig.TxtFuncMap()
+
+// Render renders a template into io.Writer
+func Render(content []byte, data interface{}, wr io.Writer) error {
+	t, err := templ.New("default").Funcs(funcMap).Parse(string(content))
+
+	if err != nil {
+		return err
+	}
+
+	t.Option("missingkey=error")
+
+	err = t.Execute(wr, data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RenderFile renders a template file into io.Writer
+func RenderFile(filename string, data interface{}, wr io.Writer) error {
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return err
+	}
+
+	t, err := templ.New("default").Funcs(funcMap).Parse(string(content))
+
+	if err != nil {
+		return err
+	}
+
+	t.Option("missingkey=error")
+
+	err = t.Execute(wr, data)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
